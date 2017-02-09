1f8b191cf714d5bb0e915d8ef504fc2fe22b8de9
Karina Widhia Nugrahini <karinwidhia@gmail.com>
2017-02-09 14:57:34 +0700

    Add project files.

---
+++ aspmvc2/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using aspmvc2.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aspmvc2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+
+        [AuthorizeRole("Admin")]
+        public ActionResult AdminOnly()
+        {
+            return View();
+        }
+
+        public ActionResult UnAuthorized()
+        {
+            return View();
+        }
+    }
+}
---
+++ aspmvc2/Security/AuthorizeRoleAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web.Mvc;
+using aspmvc2.Models.DB;
+using aspmvc2.Models.EntityManager;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace aspmvc2.Security
+{
+    public class AuthorizeRoleAttribute : AuthorizeAttribute
+    {
+        private readonly string[] userAssignedRoles;
+        public AuthorizeRoleAttribute(params string[] roles)
+        {
+            this.userAssignedRoles = roles;
+        }
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            bool authorize = false;
+            using (DemoDBEntities db = new DemoDBEntities())
+            {
+                UserManager UM = new UserManager();
+                foreach (var roles in userAssignedRoles)
+                {
+                    authorize = UM.IsUserInRole(httpContext.User.Identity.Name, roles);
+                    if (authorize)
+                        return authorize;
+                }
+            }
+            return authorize;
+        }
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectResult("~/Home/UnAuthorized");
+        }
+    }
+}
